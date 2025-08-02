6ec4feda41be0ed9bd23d30642303b4f12e05365
Stefan Chambov <stefanchambov@Stefans-MacBook-Air.local>
2025-08-02 21:38:52 +0200

    BE: I LOVE PRISMA <3

---
+++ backend/notestoself.md
@@ -0,0 +1,4 @@
+_Sample commands after each update of the prisma schema_
+npx prisma migrate dev --name add-user-model
+npx prisma generate
+!add-user-model is dynamic and should be changed similar to a commit message!
---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
