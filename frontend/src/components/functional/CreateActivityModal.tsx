5f7a50f899bbce401236cf83031b2e3857605029
stefan5441 <stefan.cambov2000@gmail.com>
2025-09-03 22:15:54 +0200

    FE: Add new activity modal, styling only for now

--- frontend/src/components/functional/CreateActivityModal.tsx
+++ frontend/src/components/functional/CreateActivityModal.tsx
@@ -1,7 +1,61 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "../ui/dialog";
+import { Select, SelectTrigger, SelectContent, SelectItem, SelectValue } from "../ui/select";
+import { colorMap } from "../styling/utils";
+
 export const CreateActivityModal = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-zinc-800/80">
-      <div className="bg-sky-200 text-zinc-800 p-6 rounded-2xl shadow-xl">test</div>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="secondary">New activity</Button>
+      </DialogTrigger>
+
+      <DialogContent className="w-[50vw] max-w-xl flex flex-col gap-8">
+        <DialogHeader>
+          <DialogTitle>Create an activity</DialogTitle>
+        </DialogHeader>
+
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="activityName">Activity name</Label>
+            <Input id="activityName" placeholder="Competitive programming" />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="activityColor">Activity color</Label>
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a color" />
+              </SelectTrigger>
+              <SelectContent>
+                {Object.entries(colorMap).map(([colorName, colorClass]) => (
+                  <SelectItem key={colorName} value={colorName} className="flex items-center gap-2">
+                    <span className={`w-4 h-4 rounded-full ${colorClass}`}></span>
+                    {colorName}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+
+        <DialogFooter className="flex justify-end gap-2">
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit">Save</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
--- frontend/src/pages/Home/HomeLoggedIn.tsx
+++ frontend/src/pages/Home/HomeLoggedIn.tsx
@@ -8,6 +8,7 @@ import { Container } from "../../components/styling/Container";
 import { ActivityCard } from "@/components/styling/ActivityCard";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { FaUser } from "react-icons/fa";
+import { CreateActivityModal } from "@/components/functional/CreateActivityModal";
 
 export const HomeLoggedIn = () => {
   const { data: userData } = useQuery({
@@ -21,43 +22,41 @@ export const HomeLoggedIn = () => {
 
   const formattedDate = `${weekday}, ${dayMonth}`;
   return (
-    <>
-      <Container size="medium" className="flex flex-col justify-between min-h-screen">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <Header content={`Hi, ${userData?.username}`} size="medium" bold />
-            <Text content={formattedDate} size="small" />
-          </div>
-
-          <Button variant={"default"}>
-            <FaUser />
-          </Button>
+    <Container size="medium" className="flex flex-col justify-between min-h-screen">
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <Header content={`Hi, ${userData?.username}`} size="medium" bold />
+          <Text content={formattedDate} size="small" />
         </div>
 
-        <Card>
-          <CardHeader>
-            <CardTitle>Recent activities</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-nowrap overflow-x-auto gap-4 pb-4">
-            <ActivityCard
-              activityName={"Competitive Programming"}
-              activityLengthInSeconds={2000}
-              activityColor="PURPLE"
-            />
-            <ActivityCard activityName={"Running"} activityLengthInSeconds={6283} activityColor="BLUE" />
-            <ActivityCard
-              activityName={"Competitive Programming"}
-              activityLengthInSeconds={2322}
-              activityColor="PURPLE"
-            />
-          </CardContent>
-        </Card>
+        <Button variant={"default"}>
+          <FaUser />
+        </Button>
+      </div>
 
-        <div className="flex justify-center gap-2">
-          <Button variant={"default"}>Record an activity</Button>
-          <Button variant={"secondary"}>New activity</Button>
-        </div>
-      </Container>
-    </>
+      <Card>
+        <CardHeader>
+          <CardTitle>Recent activities</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-nowrap overflow-x-auto gap-4 pb-4">
+          <ActivityCard
+            activityName={"Competitive Programming"}
+            activityLengthInSeconds={2000}
+            activityColor="PURPLE"
+          />
+          <ActivityCard activityName={"Running"} activityLengthInSeconds={6283} activityColor="BLUE" />
+          <ActivityCard
+            activityName={"Competitive Programming"}
+            activityLengthInSeconds={2322}
+            activityColor="PURPLE"
+          />
+        </CardContent>
+      </Card>
+
+      <div className="flex justify-center gap-2">
+        <Button variant={"default"}>Record an activity</Button>
+        <CreateActivityModal />
+      </div>
+    </Container>
   );
 };
